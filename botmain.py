4decc25edcacef2e4828d51a64510bf827936b36
Phyllis Poh <phypoh@gmail.com>
2018-08-09 17:49:04 +0800

    First commit

---
+++ botmain.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 17:35:25 2018
+
+@author: phypoh
+"""
+
+import os 
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+    
+@bot.command()
+async def hello():    
+    await bot.say("Hola. <:wave:> ")
+    
+#@bot.listen()
+
+@asyncio.coroutine
+def on_message(self, message):
+    if "rn" in message:
+        await self.bot.say("Right now! <:rn:> ")
+    
+bot.run(os.getenv('BOT_TOKEN'))
