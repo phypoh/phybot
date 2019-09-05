7550250367afd92fb344f44337a1add03be4a64b
phypoh <phypoh@gmail.com>
2019-09-05 16:02:22 +0200

    Testing API Cog

---
+++ lolApiCog.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sept  5 17:35:25 2019
+
+@author: phypoh
+"""
+
+#==============================================================================
+# LEAGUE API COMMANDS
+#==============================================================================
+
+import discord
+from discord.ext import commands
+import os
+import requests
+
+
+class apiCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.parameters = {"api_key": os.getenv('API_KEY')}
+
+        @commands.command()
+        async def spy(input_name: str):
+            request = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + input_name,
+                                   self.parameters)
+            await bot.say(request.text)
--- main.py
+++ main.py
@@ -9,12 +9,14 @@ Created on Thu Aug  9 17:35:25 2018
 import os 
 import discord
 from discord.ext import commands
-import requests
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
+    bot.load_extension("lolApiCog")
+    print("lolApiCog loaded")
+
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -31,12 +33,45 @@ async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
-parameter = {"api_key": os.getenv('API_KEY')}
 
 @bot.command()
-async def spy(input_name : str):
-    request = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + input_name, parameter)
-    await bot.say(request.text)
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    """Is the bot cool?"""
+    await bot.say('Yes, the bot is cool.')
+
 
 
 bot.run(os.getenv('BOT_TOKEN'))
